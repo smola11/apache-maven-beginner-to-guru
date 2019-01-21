20403d4bf5c9ee7ce627e7ff67d135febe9a122f
Maciej  Smolarz <smola11@o2.pl>
2019-01-21 20:13:51 +0100

    Added Mapper

--- multi-module-project-maven-10/web-app/src/main/java/controllers/UserController.java
+++ multi-module-project-maven-10/web-app/src/main/java/controllers/UserController.java
@@ -1,11 +1,12 @@
 package controllers;
 
+import com.maciej.converters.UserMapper;
 import entities.User;
 import guru.springframework.domain.UserCommand;
 
 public class UserController {
 
     public User getUser(UserCommand userCommand) {
-        return new User();
+        return UserMapper.INSTANCE.userCommandToUser(userCommand);
     }
 }
